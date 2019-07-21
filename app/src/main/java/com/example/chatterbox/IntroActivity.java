68c125c50a36e3fe0104456871bef738bf40a925
monishkumarv <kumarmessi44@gmail.com>
2019-07-21 18:28:19 +0530

    Intro added

--- app/src/main/java/com/example/chatterbox/IntroActivity.java
+++ app/src/main/java/com/example/chatterbox/IntroActivity.java
@@ -1,24 +1,53 @@
 package com.example.chatterbox;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public class IntroActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
+    private ImageView mIntroImg;
+    private TextView mTitle;
+    Animation mAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        mIntroImg=findViewById(R.id.intro_img);
+        mTitle=findViewById(R.id.title);
 
-        mAuth = FirebaseAuth.getInstance();
-        Intent intent = new Intent(IntroActivity.this,MainActivity.class);
-        startActivity(intent);
+        mAnimation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.fadein);
+        mIntroImg.startAnimation(mAnimation);
+        mAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent mainIntent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
+
+
 }
